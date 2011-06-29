6be82399f43e7e7a47358522c9a6a886020d3170
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2011-06-29 17:16:57 -0400

    init_opts.m: Wrote function documentation and commented out inapplicable values (such as displayFig)

--- src/vbem/init_opts.m
+++ src/vbem/init_opts.m
@@ -1,9 +1,23 @@
-function vb_opts = get_hFRET_vbopts();
+function vbem_opts = init_opts();
+% vb_opts = init_opts(varargs)
+%
+% Initialize default VBEM algorithm options
+%
+% Output
+% ------
+%
+% 	vb_opts : struct
+%		.maxIter
+%			Maximum number of VBEM iterations
+% 		.threshold
+%			Convergence threshold. VBEM iteration will stop when  
+%           relative increase in evidence drops below this value.
 
-% set the vb_opts for VBEM
-vb_opts.maxIter = 100;
-vb_opts.threshold = 1e-5;
-vb_opts.displayFig = 0;
-vb_opts.DisplayItersToConverge = 1;
-vb_opts.displayNrg = 0;
-vb_opts.displayIter = 0;
+vbem_opts.maxIter = 100;
+vbem_opts.threshold = 1e-5;
+
+% TODO: no longer sure this is still needed
+%vb_opts.displayFig = 0;
+%vb_opts.DisplayItersToConverge = 1;
+%vb_opts.displayNrg = 0;
+%vb_opts.displayIter = 0;
