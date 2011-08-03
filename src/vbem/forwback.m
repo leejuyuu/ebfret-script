6799ca27859fd3c0151b3439ec99bf1f938c0743
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2011-08-03 10:44:26 -0400

    Added forwback.m
    * Rewrite of Jonathan Bronson's forwbackFRET.m (which in turn was
      modified from Matthew Beal)
    
    * Removed outputs wa, wpi, xbar, S, Nk (these are technically not part
      of the forward backward algorithm and are now calculated inside
      vbem.m)
    
    * Added outputs gamma(t,k) and xi(t,k,l). Note that Xi in old code
      was summed over t whereas new output xi is not
    
    * Renamed scale -> c and changed output lnZ = sum(log(scale)) to
      px = prod(c)

---
+++ src/vbem/forwback.m
@@ -0,0 +1,96 @@
+function  [gamma, xi, px] = forwback(px_z, A, pi)
+% [gamma, xi, px] = forwback(px_z, A, pi)
+%          
+% Performs forward-backward message passing for HMMs.
+% 
+% Inputs
+% ------
+%
+%   px_z : (T x K) 
+%       Observation likelihood p(x(t) | z(t)=k, theta) = px_z(t, k) 
+%       given the latent state at each time point.
+%
+%   A : (K x K) 
+%       Transition probabilities 
+%
+%         p(z(t+1)=l | z(t)=k, theta)  =  A(k, l)
+%
+%   pi : (K x 1) 
+%       Prior probabilities for states
+%
+%         p(z(1)=k | theta)  =  pi(k)
+%
+% Outputs
+% -------
+%
+%   gamma : (T x K)
+%       Posterior probabilities for states
+%         p(z(t)=k | x(1:T))  =  gamma(t, k)
+%
+%   xi : (T-1 x K x K)
+%       Posterior joint probabilities for states
+%         p(z(t+1)=l, z(t)=k | x(1:T))  =  xi(t, k, l)
+%
+%   px : float
+%       Normalization constant p(x(1:T) | theta)
+%
+% Jan-Willem van de Meent (modified from Matthew Beal and Jonathan Bronson)
+% $Revision: 1.00 $  $Date: 2011/08/03$
+
+% get dimensions
+K = size(A, 1);
+T = size(px_z, 1);
+
+% Forward backward message passing  
+%                           
+% alpha(t, k) = p(x(1:t), z(t)=k) / p(x(1:t))
+% beta(t, k) = p(x(t+1:T) | z(t)=k) / p(x(t+1:T) | x(1:t))
+% scale(t, k) = p(x(t) | x(1:t-1))
+alpha = zeros(T,K);
+beta = zeros(T,K);
+c = zeros(T,1);
+
+% Forward pass (with scaling)
+alpha(1,:) = pi' .* px_z(1, :);
+c(1) = sum(alpha(1, :));
+alpha(1,:) = alpha(1, :) ./ c(1); 
+for t=2:T
+  % alpha(t, k) = sum_l p(x(t) | z(t)=k) A(l, k) alpha(t-1, l) 
+  alpha(t,:) = (alpha(t-1, :) * A) .* px_z(t, :); 
+  % c(t) = p(x(t) | x(1:t-1)) = sum_l alpha(t, l)
+  c(t) = sum(alpha(t, :));
+  % normalize alpha by factor p(x(t) | p(x(1:t-1)))
+  alpha(t,:) = alpha(t,:) ./ c(t); 
+  % note: prod(c(1:t)) = p(x(1:t))
+end
+  
+% Backward pass (with scaling)
+beta(T,:) = ones(1, K);
+for t=T-1:-1:1
+  % beta(t, k) = sum_l p(x(t+1) | z(t)=l) A(k, l) beta(t+1, l) 
+  beta(t, :) = (beta(t+1,:) .* px_z(t+1,:)) * A' / c(t+1);  
+  % note: prod(c(t+1:T)) = p(x(t+1:T) | x(1:t))
+end
+
+% Posterior probabilities for states
+%
+% gamma(t, k) = p(z(t) | x(1:T))
+%             = p(x(1:t), z(t)) p(x(t+1:T) | z(t)) / p(x(1:T))
+%             = alpha(t) beta(t) 
+gamma = alpha .* beta;
+
+% Posterior transition joint probabilities
+%
+% xi(t, k, l) = p(z(t)=k, z(t+1)=l | x(1:T))
+%             = alpha(t, k) A(k,l) px_z(t+1, l) beta(t+1, l) / c(t+1)
+pxz_b_c = bsxfun(@times, 1./c(2:T), beta(2:T, :)) .* px_z(2:T, :);
+xi = bsxfun(@times, ...
+            reshape(alpha(1:T-1,:), [T-1 K 1]), ...
+            bsxfun(@times, ...
+                   reshape(A, [1, K, K]), ...
+                   reshape(pxz_b_c, [T-1 1 K])));
+
+% Evidence
+%
+% px = p(x(1:T)) = Prod_t c(t)
+px = prod(c);
