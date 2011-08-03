4da13403d7e31a0f372054030cfab480e4674cfe
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2011-08-03 09:37:06 -0400

    init_u.m: Labeled 1.0

--- src/vbem/init_u.m
+++ src/vbem/init_u.m
@@ -1,35 +1,145 @@
-function PriorPar = get_priors(K,h)
+function u = init_u(K,varargin)
+% u = init_u(K, varargin)
+%
+% Initializes a set of hyperparameters u used to define the prior 
+% distribution p(theta | u).
+%
+%
+% Inputs
+% ------
+%
+%   K : int
+%       Number of states.
+%
+%
+% Variable Inputs
+% ---------------
+%
+% 'mu' : string
+%   Specifies type of prior on levels of states.
+%       
+%       'linear'
+%           Evenly spaced values mu = (1:K) / (K+1)
+%
+%       'random'
+%           Take K random values between 0 and 1, with a minimum
+%           separation of 0.02.
+%       
+%       'rand_ends'
+%           Evenly space values between a random minimum and maximum.
+%
+% Outputs
+% -------
+%
+%   u : struct
+%       Hyperparameters for prior distribution p(theta | u)
+%
+%       .A (KxK)
+%           Dirichlet prior for each row of transition matrix
+%       .pi (Kx1)
+%           Dirichlet prior for initial state probabilities
+%       .mu (KxD)
+%           Gaussian-Wishart posterior for state means 
+%       .beta (Kx1)
+%           Gaussian-Wishart posterior for state occupation count
+%       .W (KxDxD)
+%           Gaussian-Wishart posterior for state precisions
+%       .nu (Kx1)
+%           Gaussian-Wishart posterior for degrees of freedom
+%           (must be equal to beta+1)
+%
+%
+% Jan-Willem van de Meent (modified from Jonathan Bronson)
+% $Revision: 1.00 $  $Date: 2011/08/03$
 
-% Hyperparameters for 1D
-PriorPar.upi = ones(1,K);
+% TODO
+% ----
+%
+% * This function currently does not support D=2 donor/acceptor inference
+%   (have no idea how to set priors for mu for this case)
 
-% mus are a bit complicated
-if h == 1
-    % first try spreding states evenly between 0 and 1
-    mu = (1:K)/(K+1);
-else
-    % then try at random, subject to some constraints below
-    mu = rand(1,K);
-end
 
-% this just makes later analysis and calculations easier
-mu = sort(mu);
 
-% smoth things out a bit
-if mod(h,2)
-    % make things uniform between mu(min) and mu(max)
-    mu = linspace(mu(1),mu(end),K);
-else 
-    % no point in having 2 states directly on top of each other
-    while min(mu) < 0.02
-        mu = rand(1,K);
-        mu = sort(mu);
+% Parse variable arguments
+mu_type = 'random'
+for i = 1:length(varargin)
+    if isstr(varargin{i})
+        switch lower(varargin{i})
+        case {'mu'}
+            mu_type = lower(varargin{i+1});
+            valid_types = {'linear', 'random', 'rand_ends'};
+            % check whether mu_type has a valid value
+            valid = arrayfun(@(t) strcmp(mu_type, valid_types(i)), ... 
+                             1:length(mu_types));
+            if ~any(valid)
+                err = MException('init_u:invalid_mu_type', ...
+                                 '[init_u] Error: ''mu'' must be one of {''linear'', ''random'', ''randend''}');
+                throw(err)
+            end 
+        end
     end
-end
+end 
 
-PriorPar.mu = mu;
-PriorPar.beta = 1*ones(K,1);
-% might want to change this later
-PriorPar.W = 50*ones(1,K);
-PriorPar.v = PriorPar.beta + 1;
-PriorPar.ua = ones(K);
+% Prior for initial probabilities 
+%
+%   p(z(1) = k | u.pi)  =  pi(k)  ~  Dir(u.pi)
+%
+% make this uniform
+u.pi = ones(1,K);
+
+% Prior for transition probabilities 
+%
+%   p(z(t)=l | z(t-1)=k)  =  A(k, l)  ~  Dir(u.A)
+%
+% make this uniform too.
+u.A = ones(K);
+
+% Prior for emission model state levels and precision
+%
+%   lambda(k) ~ Wishart(u.nu(k), u.W(k))
+%   mu(k) ~ Normal(u.mu(k), 1/(u.beta(k) lambda(k)))
+%
+%   p(x(t) | z(t)=k)  =  Normal(mu(k), 1/lambda(k))
+
+% mu: choice between 'linear', 'random', and 'rand_ends'
+switch(mu_type)
+    case {'linear'}
+        % linearly space states between 1/(K+1) and K/(K+1)
+        mu = (1:K)'/(K+1);
+
+    case {'random'}
+        % ensure spacing of states is at least  0.02
+        dmu = zeros(K, K);
+        while min(dmu(:) < 0.02)
+            % generate a set of random mu values
+            mu = rand(K, 1);
+            % calculate spacing between states
+            dmu = abs(bsxfun(@minus, mu, mu'));
+        end
+        % output values in ascending order
+        mu = sort(mu);
+
+    case {'rand_ends'}
+        % ensure spacing of states is at least  0.02
+        dmu = zeros(K, K);
+        while min(dmu(:) < 0.02)
+            % generate a set of random mu values
+            mu = rand(K, 1);
+            % now space evenly between min and max
+            mu = linspace(min(mu), max(mu), K);
+            % calculate spacing between states
+            dmu = abs(bsxfun(@minus, mu, mu'));
+        end
+end        
+
+% beta: uniform, one count for each state 
+u.beta = ones(K, 1);
+% nu: fully determined by beta
+u.nu = u.beta + 1;
+
+% W: set uniformly to 400 / nu. This implies implies an expectation value for 
+% the emission noise of 0.05
+%
+%    <lambda> = nu * W = 400
+%    <sigma> = 1./sqrt(<lambda>) = 0.05
+u.W = 400 ./ u.nu; 
