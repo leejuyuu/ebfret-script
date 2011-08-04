c07772a382e8d9c97f3ab1f3f5bcb22926065858
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2011-08-04 16:17:19 -0400

    vbem.m: vectorized calculation of D_kl_mu_L
    * Reverted commit d483d88c56940dc91078bad5de4bdb71f9004012
      that replaced calculation of D_kl_mu_L with a call to kl_normwish
    
    * Replaced for loop over k with vectorized calculations of
      constituent terms
    
    * Take advantage of simplicifactions in calculation in D=1 case

--- src/vbem/vbem.m
+++ src/vbem/vbem.m
@@ -155,7 +155,7 @@ function [w, L, stat] = vbem(x, w0, u, options)
 %       PhD Thesis, University College London, 2003
 %       http://www.cse.buffalo.edu/faculty/mbeal/
 
-Debug = true;
+Debug = false;
 
 % set defaults for options that are not supplied
 if nargin < 4
@@ -274,10 +274,6 @@ for it = 1:options.maxIter
     %                              + D_kl(q(A) || p(A)) 
     %                              + D_kl(q(pi) || p(pi))
 
-    % D_KL(q(mu,L) || p(mu,L))
-    D_kl_mu_L = kl_normwish(w.mu, w.beta, w.W, w.nu, ...
-                            u.mu, u.beta, u.W, u.nu);
-
     % D_kl(q(pi) || p(pi)) = sum_l (w.pi(l) - u.pi(l)) 
     %                              (psi(w.pi(l)) - psi(u.pi(l)))
     D_kl_pi = kl_dir(w.pi, u.pi);
@@ -286,7 +282,81 @@ for it = 1:options.maxIter
     %                                (psi(w.A(k,l)) - psi(u.A(k,l)))
     D_kl_A = kl_dir(w.A, u.A);
 
+    
+    % Calculate Dkl(q(mu, L | w) || p(mu, L | u)) 
 
+    % pre-compute some terms so calculation can be vectorized
+    % (this was done after profiling)
+    if D > 1
+        for k = 1:K
+            log_det_W_u(k) = log(det(u.W(k, :, :)));
+            log_det_W_w(k) = log(det(w.W(k, :, :)));
+            E_Tr_Winv_L(k) = trace(inv(u.W(k, :, :) ...
+                                       * w.W(k, :, :)));
+            dmWdm = (w.mu(k,:) - u.mu(k,:))' ...
+                    * w.W(k,:,:) ...
+                    * (w.mu(k,:) - u.mu(k,:));
+        end
+    else
+        % for the most common D=1 case we don't need 
+        % calls to det, inv, mtimes
+        log_det_W_u = log(u.W);
+        log_det_W_w = log(w.W);
+        E_Tr_Winv_L = w.W ./ u.W;
+        dmWdm = w.W .* (w.mu-u.mu).^2;
+    end 
+
+    % Log norm const Log[B(W, nu)] for Wishart (CB B.79)
+    log_B = @(log_det_W, nu) ...
+            - (nu / 2) .* log_det_W ...
+            - (nu * D / 2) * log(2) ...
+            - (D * (D-1) / 4) * log(pi) ...
+            - sum(gammaln(0.5 * bsxfun(@minus, nu + 1, (1:D))), 2);
+
+    % E_q[q(mu, L | w)]
+    % =
+    % 1/2 E_q[log |L|]
+    % + D log(w.beta / (2*pi)) 
+    % - 1/2 D
+    % + log(B(w.W, w.nu))
+    % + 1/2 (w.nu - D - 1) * E_q[log |L|]
+    % - 1/2 w.nu D
+    E_log_NW_w = 0.5 * E_ln_det_L' ...        
+                 + 0.5 * D * log(w.beta ./ (2*pi)) ...
+                 - 0.5 * D ...
+                 + log_B(log_det_W_w, w.nu) ...
+                 + 0.5 * (w.nu-D-1) .* E_ln_det_L' ...
+                 - 0.5 * w.nu * D;
+
+    % E_q[log[Norm(mu | u.mu, u.beta L)]
+    % =
+    %   1/2 D log(u.beta / 2 pi) 
+    %   + 1/2 E_q[log |L|]
+    %   - 1/2 u.beta (D / w.beta + E_q[(mu-u.mu)^T L (mu-u.mu)])  
+    E_log_Norm_u = 0.5 * (D * log(u.beta / (2*pi)) ...
+                          + E_ln_det_L' ... 
+                          - D * u.beta ./ w.beta ...
+                          - u.beta .* w.nu .* dmWdm);
+
+    % E_q[log[Wish(L | u.W, u.nu)]
+    % =
+    % log(B(u.W, u.nu)) 
+    % + 1/2 (u.nu - D - 1) E_q[log |L|]
+    % - 1/2 w.nu Tr[Inv(u.W) * w.W]
+    E_log_Wish_u = log_B(log_det_W_u, u.nu) ...
+                   + 0.5 * (u.nu - D - 1) .* E_ln_det_L' ...
+                   - 0.5 * w.nu .* E_Tr_Winv_L;
+
+    % E_q[p(mu, L | u)]
+    % = 
+    % E_q[log[Norm(mu | u.mu, u.beta L)]
+    % + E_q[log[Wish(L | u.W, u.nu)]
+    E_log_NW_u = E_log_Norm_u + E_log_Wish_u;
+
+    % Dkl(q(mu, L | w) || p(mu, L | u)) 
+    % =
+    % E_q[log q(mu, L | w)] - E_q[log p(mu, L | u)]
+    D_kl_mu_L = E_log_NW_w - E_log_NW_u;    
 
     % L = ln(Z) - D_kl(q(theta | w) || p(theta | u))
     L(it) = log(Z) - sum(D_kl_mu_L) - sum(D_kl_A) - sum(D_kl_pi);
