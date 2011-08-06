7562a18d45368f2fb9f473fdffb3f1124cdfea2d
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2011-08-06 13:38:19 -0400

    hstep_ml.m: Refactored and fixed
    * Fixed bug: expectation values were not normalized by N
    * Now accepts refactored u and w variables
    * No longer returns MAP theta parameters.
    * Weighing of traces disabled
    * TODO: needs a bit of clean-up

--- src/hmi/hstep_ml.m
+++ src/hmi/hstep_ml.m
@@ -1,4 +1,6 @@
-function [PostHPar PostPar] = hstep_ml(out, PriorPar, varargin)
+function u_new = hstep_ml(w, u)
+% u_new = hstep_ml(w, u, varargin) 
+%
 % Hyper parameter updates for Hierarchical Model Inference (HMI)
 % on a single-molecule FRET dataset.
 %
@@ -24,55 +26,39 @@ function [PostHPar PostPar] = hstep_ml(out, PriorPar, varargin)
 % Inputs
 % ------
 %
-% out (Nx1 Cell)
-%   VBEM output for each trace (see VBEM source for further info)
+%   w : struct (N x 1)
+%       Variational parameters of approximate posterior distribution 
+%       for parameters q(theta | w), for each of N traces 
 %
-% PriorPar (Struct)
-%   Hyperparameters that define priors, as passed to VBEM
+%       .mu (K x D)
+%           Normal-Wishart prior - state means 
+%       .beta (K x 1)
+%           Normal-Wishart prior - state occupation count
+%       .nu (K x 1)
+%           Normal-Wishart prior - degrees of freedom
+%           (must be equal to beta+1)
+%       .W (K x D x D)
+%           Normal-Wishart prior - state precisions
+%       .A (K x K)
+%           Dirichlet prior for each row of transition matrix
+%       .pi (K x 1)
+%           Dirichlet prior for initial state probabilities
 %
-%   .upi (1xK)
-%       Dirichlet prior for initial state probabilities
-%   .ua (KxK)
-%       Dirichlet prior for each row of transition matrix
-%   .mu (1xK)
-%       Gaussian-Gamma/Wishart prior for state means 
-%   .beta (Kx1)
-%       Gaussian-Gamma/Wishart prior for state occupation count 
-%   .W (DxDxK)
-%       Gaussian-Gamma/Wishart prior for state precisions
-%   .v (Kx1)
-%       Gaussian-Gamma/Wishart prior for degrees of freedom
-%
-% Variable Inputs
-% ---------------
-%
-% 'WeighTraces' (boolean)
-%   Weigh contributions to updates by evidence (True by default)
+%   u : struct 
+%       Hyperparameters for the prior distribution p(theta | u)
+%       (same fields as w)
 %
 % Outputs
 % -------
 %
-% PostHpar (Struct)
-%   Updated hyperparameters (same fields as PriorPar)
-%
-% PostPar
-%   Maximum likelihood parameters associated with updated priors.
-%
-%   .pi (1xK)
-%       Initial state probabilities
-%   .A (KxK)
-%       Transition matrix
-%   .m (1xK)
-%       Emission FRET levels
-%   .sigma (1xK)
-%       Emission FRET standard deviation 
-%
-% TODO: this currently does not work for 2D donor/acceptor inference
+%   u_new : struct 
+%       Updated hyperparameters
 %
 % Jan-Willem van de Meent
-% $Revision: 1.00$  $Date: 2011/05/21$
+% $Revision: 1.10$  $Date: 2011/08/04$
 
-
+% TODO: this currently does not work for 2D donor/acceptor inference
+%
 % Explanation of Updates
 % ----------------------
 %
@@ -98,7 +84,7 @@ function [PostHPar PostPar] = hstep_ml(out, PriorPar, varargin)
 % The system of equations Grad Sum_n L^n = 0 can now be solved
 % from the above identities, resulting in the following updates
 %
-% {mu, lambda} ~ NormalGamma(mu, lambda | u.mu, u.beta, u.nu, u.W)
+%   (mu, lambda) ~ NormalGamma(mu, lambda | u.mu, u.beta, u.nu, u.W)
 %
 %   u.beta  =  u.nu - 1
 %   u.mu  =  E[mu lambda] / E[lambda]
@@ -116,58 +102,36 @@ function [PostHPar PostPar] = hstep_ml(out, PriorPar, varargin)
 %
 %   psi(Sum u.pi) - psi(u.pi) = -E[log pi] 
 
-% parse variable arguments
-WeighTraces = true;
-for i = 1:length(varargin)
-    if isstr(varargin{i})
-        switch lower(varargin{i})
-        case {'weightraces'}
-            WeighTraces = varargin{i+1};
-        end
-    end
-end 
-
 % Get dimensions
-N = length(out);
-K = length(out{1}.Wpi);
+N = length(w);
+K = length(w(1).pi);
 
-% Prior parameters to struct form
-u_old = struct();
-u_old.pi = PriorPar.upi(:);
-u_old.A = PriorPar.ua;
-u_old.mu = PriorPar.mu(:);
-u_old.beta = PriorPar.beta(:);
-u_old.nu = PriorPar.v(:);
-u_old.W = PriorPar.W(:);
+% % get weights for updates
+% if WeighTraces
+%   % weigh by evidence of each trace
+%   weights = normalise(cellfun(@(o) o.F(end), out));
+% else
+%   weights = ones(N,1) / N;
+% end
 
-% get weights for updates
-if WeighTraces:
-  % weigh by evidence of each trace
-  weights = normalise(cellfun(@(o) o.F(end), out))
-else:
-  weights = ones(N,1) / N
-
-% Get posterior hyperparameters w = u' for q(theta) 
-w = struct('pi', cellfun(@(o) o.Wpi(:), out, 'UniformOutput', false), ...
-           'A', cellfun(@(o) o.Wa, out, 'UniformOutput', false), ...
-           'mu', cellfun(@(o) o.m(:), out, 'UniformOutput', false), ...
-           'beta', cellfun(@(o) o.beta(:), out, 'UniformOutput', false), ...
-           'W', cellfun(@(o) o.W(:), out, 'UniformOutput', false), ...
-           'nu', cellfun(@(o) o.v(:), out, 'UniformOutput', false), ...
-           'weights', arrayfun(@(w) w, weights,'UniformOutput', false))
+% Dummy weights
+for n = 1:N
+  w(n).wt = 1;
+end
 
 % Init struct for updated params
+u_old = u;
 u = struct();
 
 % (mu, lambda) ~ Normal-Gamma
 % Expectation Value eta1: E[lambda] = w.nu * w.W
 E_l = arrayfun(@(w) w.nu .* w.W .* w.wt, w, 'UniformOutput', false);
-E_l = sum([E_l{:}], 2);
+E_l = mean([E_l{:}], 2);
 
 % (mu, lambda)
 % Expectation Value eta2: E[mu * lambda] = w.nu * w.W * w.mu
 E_ml = arrayfun(@(w) w.nu .* w.W .* w.mu .* w.wt, w, 'UniformOutput', false);
-E_ml = sum([E_ml{:}], 2);
+E_ml = mean([E_ml{:}], 2);
 
 % (mu, lambda): For expectation values of E[log g] we use 
 %
@@ -188,7 +152,7 @@ E_log_g = arrayfun(@(w) -0.5 * w.wt .* (1 ./ w.beta ...
                                + log(pi ./ w.W) ...
                                - psi(w.nu/2)), ...
                    w, 'UniformOutput', false);
-E_log_g = sum([E_log_g{:}], 2);
+E_log_g = mean([E_log_g{:}], 2);
 
 % (mu, lambda): Solve for u.nu
 %
@@ -201,7 +165,12 @@ root_fun = @(nu) E_log_g ...
                           + (E_ml.^2 ./ E_l) ...
                           + log(pi .* nu ./ E_l) ...
                           - psi(nu / 2));
-u.nu = lsqnonlin(root_fun, u_old.nu, 1 + zeros(size(u_old.nu)));
+opts = optimset('display', 'off');
+u.nu = lsqnonlin(root_fun, ...
+                 u_old.nu, ...
+                 ones(size(u_old.nu)), ...
+                 Inf + zeros(size(u_old.nu)), ...
+                 opts);
 
 % (mu, lambda): Solve u.mu, u.beta and u.W
 u.mu = E_ml ./ E_l;
@@ -209,7 +178,7 @@ u.beta = u.nu - 1;
 u.W  = E_l ./ u.nu;
 
 
-% (pi) ~ Dirichlet
+% pi ~ Dirichlet
 % 
 % Expectation value E[log pi]: Use same trick as for E[log g]:
 %
@@ -217,60 +186,66 @@ u.W  = E_l ./ u.nu;
 %           = - psi(Sum w.pi) + psi(w.pi)
 E_log_pi = arrayfun(@(w) -psi(sum(w.pi)) + psi(w.pi), ...
                     w, 'UniformOutput', false);
-E_log_pi = sum([E_log_pi{:}], 2) / N;
+E_log_pi = mean([E_log_pi{:}], 2);
 
 %(pi): solve system of equations
 root_fun = @(p) E_log_pi + psi(sum(p)) - psi(p);
-u.pi = lsqnonlin(root_fun, u_old.pi, zeros(size(u_old.pi)) + eps);
+u.pi = lsqnonlin(root_fun, ...
+                 u_old.pi, ...
+                 zeros(size(u_old.pi)) + eps, ...
+                 Inf + zeros(size(u_old.pi)), ...
+                 opts);
 
 
 % A(k,:) ~ Dirichlet
 % E[log A(k,:)]: Same as E[log pi]
 E_log_A = arrayfun(@(w) w.wt * bsxfun(@plus, -psi(sum(w.A,2)), psi(w.A)), ...
                     w, 'UniformOutput', false);
-E_log_A = sum(cat(3, E_log_A{:}), 3);
+E_log_A = mean(cat(3, E_log_A{:}), 3);
 
 %A(k,:): solve system of equations 
 u.A = zeros(size(u_old.A));
 for k = 1:K
   root_fun = @(Ak) E_log_A(k,:) + psi(sum(Ak)) - psi(Ak);
-  u.A(k, :) = lsqnonlin(root_fun, u_old.A(k, :), zeros(size(u_old.A(k, :))) + eps);
+  u.A(k, :) = lsqnonlin(root_fun, ...
+                        u_old.A(k, :), ...
+                        zeros(size(u_old.A(k, :))) + eps, ...
+                        Inf + zeros(size(u_old.A(k, :))), ...
+                        opts);
 end
 
-% Pack results back into legacy form
-PostHPar = struct();
-PostHPar.upi = u.pi';
-PostHPar.ua = u.A;
-PostHPar.mu = u.mu';
-PostHPar.beta = u.beta;
-PostHPar.v = u.nu;
-PostHPar.W = u.W';
+u_new = struct('mu', u.mu, ...
+               'beta', u.beta, ...
+               'nu', u.nu, ...
+               'W', u.W, ...
+               'pi', u.pi, ...
+               'A', u.A);
 
-% Get Max Posterior Parameters (based on updated priors)
-% (this is a pain)
+% % Get Max Posterior Parameters (based on updated priors)
+% % (this is a pain)
 
-p = cellfun(@(o) o.Wpi(:) - u_old.pi(:), out, 'UniformOutput', false);
-p = sum([p{:}], 2);
-Nk = cellfun(@(o) o.Nk(:), out, 'UniformOutput', false);
-Nk = sum([Nk{:}], 2);
-xbar = cellfun(@(o) o.Nk(:) .* o.xbar(:), out, 'UniformOutput', false);
-xbar = sum([xbar{:}], 2) ./ Nk;
-S = cellfun(@(o) o.Nk(:) .* squeeze(o.S), out, 'UniformOutput', false);
-S = sum([S{:}], 2) ./ Nk;
-A = cellfun(@(o) o.Wa - u_old.A, out, 'UniformOutput', false);
-A = sum(cat(3, A{:}), 3);
+% p = cellfun(@(o) o.Wpi(:) - u_old.pi(:), out, 'UniformOutput', false);
+% p = sum([p{:}], 2);
+% Nk = cellfun(@(o) o.Nk(:), out, 'UniformOutput', false);
+% Nk = sum([Nk{:}], 2);
+% xbar = cellfun(@(o) o.Nk(:) .* o.xbar(:), out, 'UniformOutput', false);
+% xbar = sum([xbar{:}], 2) ./ Nk;
+% S = cellfun(@(o) o.Nk(:) .* squeeze(o.S), out, 'UniformOutput', false);
+% S = sum([S{:}], 2) ./ Nk;
+% A = cellfun(@(o) o.Wa - u_old.A, out, 'UniformOutput', false);
+% A = sum(cat(3, A{:}), 3);
 
-% get posterior over all data (based on updated priors)
-W = struct('pi', u.pi + p, ... 
-           'A', u.A + A, ...
-           'mu', (u.beta .* u.mu + Nk .* xbar) ./ (u.beta + Nk), ...
-           'beta', u.beta + Nk, ...
-           'W', 1./ (1 ./ u.W + Nk .* S + u.beta .* Nk .* (xbar - u.mu).^2 ./ (u.beta + Nk)), ...
-           'nu', u.nu + Nk);
+% % get posterior over all data (based on updated priors)
+% W = struct('pi', u.pi + p, ... 
+%            'A', u.A + A, ...
+%            'mu', (u.beta .* u.mu + Nk .* xbar) ./ (u.beta + Nk), ...
+%            'beta', u.beta + Nk, ...
+%            'W', 1./ (1 ./ u.W + Nk .* S + u.beta .* Nk .* (xbar - u.mu).^2 ./ (u.beta + Nk)), ...
+%            'nu', u.nu + Nk);
 
-% Pack MAP results
-PostPar = struct();
-PostPar.pi = normalise(W.pi);
-PostPar.A = normalise(W.A, 2); 
-PostPar.mu = W.mu;
-PostPar.sigma = 1./sqrt(W.nu .* W.W);
+% % Pack MAP results
+% w = struct();
+% w.pi = normalise(W.pi);
+% w.A = normalise(W.A, 2); 
+% w.mu = W.mu;
+% w.sigma = 1./sqrt(W.nu .* W.W);
