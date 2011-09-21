e812e25f35b0fe839560d6cd4dcf8eb75b02348a
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2011-09-21 16:50:46 -0400

    tracePlotter: modified arguments
    * Now one required argument: FRET (Nx1 cell)
    * Variable arguments: 'raw', 'vb', 'vit'
    * Plots variable number of panels depending on argument

--- gui/tracePlotter/tracePlotter.m
+++ gui/tracePlotter/tracePlotter.m
@@ -1,4 +1,4 @@
-function varargout = tracePlotter(FRET, out, x_hat, z_hat, varargin)
+function varargout = tracePlotter(FRET, varargin)
 % TRACE_PLOTTER Visualise a dataset of single-molecule FRET traces 
 % along results ofwith VBEM inference.
 %
@@ -8,37 +8,79 @@ function varargout = tracePlotter(FRET, out, x_hat, z_hat, varargin)
 %   'FRET' (1xN cell)
 %       Single-molecule FRET traces
 %
-%   'out' (1xN cell)
-%       VBEM output for each trace
-%
-%   'x_hat' (1xN cell)
-%       Viterbi path FRET level for each timepoint
-%
-%   'z_hat' (1xN cell)
-%       Viterbi path state index for each timepoint
-%
-%
 % Variable Inputs
 % ---------------
-%   
-%   Any variable arguments are passed to tracePlotterGUI.
+%
+%   'raw' (1xN cell)
+%		Donor/Acceptor signals
+%
+%   'vb' (1xN struct, optional)
+%       VBEM output for each trace
+%
+%   'vit' (1xN struct, optional)
+%       Viterbi paths
+%
+%   Any other variable arguments are passed to tracePlotterGUI.
 %
 % Jan-Willem van de Meent
 % $Revision: 1.00 $  $Date: 2011/05/16$
 
 % Last Modified by GUIDE v2.5 16-May-2011 15:36:13
 
-args = struct('FRET', {FRET{:}}, ...
-              'out', {out{:}}, ...
-              'x_hat', {x_hat{:}}, ...
-              'z_hat', {z_hat{:}});
+% concatenate args into single struct array
 
-if nargin > 4
-    varargin = {varargin{:}, args};
-else
-    varargin = {args};
+N = length(FRET)
+for n = 1:N
+	data(n).FRET = FRET{n};
 end
 
+% loop over variable arguments
+i=1;
+while i <= length(varargin)
+	matched = false;
+    if isstr(varargin{i})
+        switch lower(varargin{i})
+        case {'raw'}
+        	matched = true;
+        	raw = varargin{i+1};
+        	for n = 1:length(raw)
+				data(n).don = raw{n}(:,1);
+				data(n).acc = raw{n}(:,2);
+			end
+            
+        case {'vb'}
+        	matched = true;
+        	vb = varargin{i+1}
+        	% add vb fields to data struct
+			fields = fieldnames(vb);
+			for f = 1:length(fields)
+				[data.(fields{f})] = vb.(fields{f})
+			end
+            
+        case {'vit'}
+        	matched = true;
+        	vit = varargin{i+1}
+        	% add vit fields to data struct
+			fields = fieldnames(vit);
+			for f = 1:length(fields)
+				[data.(fields{f})] = vit.(fields{f})
+			end
+		end
+		        
+        if matched
+        	% strip arguments from varargin 
+        	% (so they are not passed to tracePlotterGUI)
+        	I = length(varargin);
+        	msk = ((1:I)~=i) & ((1:I)~=(i+1));
+        	varargin = {varargin{find(msk)}};
+        else
+        	i = i+1;
+        end
+    end
+end
+
+% append data to last position of varargin cell
+varargin = {varargin{:}, data};
 
 if nargout
     [varargout{1:nargout}] = tracePlotterGUI(varargin{:});
--- gui/tracePlotter/tracePlotterGUI.m
+++ gui/tracePlotter/tracePlotterGUI.m
@@ -22,7 +22,7 @@ function varargout = tracePlotterGUI(varargin)
 % Jan-Willem van de Meent
 % $Revision: 1.00 $  $Date: 2011/05/16$
 
-% Last Modified by GUIDE v2.5 16-May-2011 15:36:13
+% Last Modified by GUIDE v2.5 19-Sep-2011 23:36:06
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 0;
@@ -51,9 +51,36 @@ function tracePlotterGUI_OpeningFcn(hObject, eventdata, handles, varargin)
 
     % Init plot data and state if not previously called
     % TODO: not sure about behaviour when gui_Singleton = 0
-    if ~isfield(handles, 'data') & isstruct(varargin{end})
+    if ~isfield(handles, 'data')
         handles.data = varargin{end};
     end 
+    
+    if ~isfield(handles, 'subPlots')
+        data = handles.data;
+        handles.subPlots = 1;
+        handles.rawPlot = 0;
+        handles.gamPlot = 0;
+        handles.K = 0;
+        if isfield(data,'don') & isfield(data,'acc')
+            handles.subPlots = handles.subPlots + 1;
+            handles.rawPlot = handles.subPlots;
+        end
+        if isfield(data,'z')
+            handles.K = max(cat(1,data.z));
+        end
+        if isfield(data,'g') & isfield(data,'xi')
+            handles.K = size(data(1).g, 2);
+            handles.subPlots = handles.subPlots + 1;
+            handles.gamPlot = handles.subPlots;
+        end
+        if handles.subPlots > 1
+            P = handles.subPlots;
+            for p = 1:P
+                handles.plotAxes(p) = subplot('position', [0.04, 0.9*(P-p)/P+0.1, 0.94, 1/P-0.1]);
+            end
+        end
+    end
+
     if ~isfield(handles, 'state')
         handles.curPlot = 1;
         handles.maxPlot = length(handles.data);
@@ -89,75 +116,122 @@ function tracePlotterGUI_PlotChanged(hObject, eventdata, handles)
         sprintf('%d / %d', handles.curPlot, ...
                            handles.maxPlot));
 
-    % update plot axes
-    cla(handles.plotAxes, 'reset');
-    axes(handles.plotAxes);
-
-    FRET = handles.data(handles.curPlot).FRET;
-    x_hat = handles.data(handles.curPlot).x_hat;
-    z_hat = handles.data(handles.curPlot).z_hat;
-    out = handles.data(handles.curPlot).out;
-    T = length(FRET);
-    K = length(out.m);
-    t = 1:T;
+    % get data
+    d = handles.data(handles.curPlot);
 
     % plot FRET signal
-    sublplot(2,1,1);
-    scatter(t, FRET, 8, [0,0,0]);
-    plot(t, FRET, '-ok');
-    hold on;
-
-    % % plot viterbi trace
-    % plot(1:T, x_hat, 'r');
-
-    % loop over states
-    c = 0.66 .* hsv(K);
-    for k = 1:K
-        msk = (z_hat == k);
-        nmsk = ones(size(msk));
-        nmsk(~msk) = NaN;
-
-        % plot x_hat
-        l = plot(t(:) .* nmsk(:), x_hat(:) .* nmsk(:), '--', 'Color', c(k,:));
-        set(l, 'LineWidth', 1.5)
-
-        % plot mu posterior confidence intervals
-        s_mu = sqrt(1 ./ (out.beta(k) .* out.W(k) .* (out.v(k) - 2)));
-        l = plot(t(:) .* nmsk(:), (x_hat(:) + 2.*s_mu) .* nmsk(:), ... 
-                 '-', 'Color', c(k,:));
-        l = plot(t(:) .* nmsk(:), (x_hat(:) - 2.*s_mu) .* nmsk(:), ...
-                 '-', 'Color', c(k,:));
-
-        % plot emission confidence intervals
-        s = 1./sqrt(out.v(k) * out.W(k));
-        l = plot(t(:) .* nmsk(:), (x_hat(:)+2.*s) .* nmsk(:), ...
-                 '-.', 'Color', c(k,:));
-        l = plot(t(:) .* nmsk(:), (x_hat(:)-2.*s) .* nmsk(:), ...
-                 '-.', 'Color', c(k,:));
-
-        % append to labels
-        mu_labels{k} = sprintf('mu_%d = %.2f +/- %.2f', k, out.m(k), s_mu);
-        sigma_labels{k} = sprintf('sigma_%d = %.2f +/- %.2f', k, s, 1./sqrt(2.*out.v(k).*out.W(k).^2));
-        Nk_labels{k} = sprintf('N_%d = %05.1f', k, out.Nk(k));
-
-        hold on;
-    end
-
-    % set axis limits
+    axes(handles.plotAxes(1));
+    cla();
+    T = length(d.FRET);
+    t = 1:T;
+    plot(t, d.FRET, '-ok');
     xlim([0, T]);
     ylim([-0.2, 1.2+eps]);
 
-    % add text labels
-    t = text(0.05*T, 1.10, sprintf('F/T = %.1f', out.F(end)/T), 'FontSize', 14);
-    t = text(0.60*T, 1, Nk_labels, 'FontSize', 14);
-    t = text(0.70*T, 1, mu_labels, 'FontSize', 14);
-    t = text(0.85*T, 1, sigma_labels, 'FontSize', 14);
+    % plot viterbi signal (if specified)
+    if isfield(d, 'x') & isfield(d, 'z')
+        K = handles.K;
+        hold on;
 
-    % plot Donor/Acceptor 
-    subplot(2,1,2);
-    
+        % loop over states
+        c = 0.66 .* hsv(K);
+        for k = 1:K
+            msk = (d.z(:) == k);
+            nmsk = ones(size(msk));
+            nmsk(~msk) = NaN;
 
-    hold off
+            % plot x_hat
+            l = scatter(t(:) .* nmsk(:), d.x(:) .* nmsk(:), 18, c(k,:), 'Filled');
+            set(l, 'LineWidth', 1.5)
+
+            % % plot mu posterior confidence intervals
+            % s_mu = sqrt(1 ./ (w.beta(k) .* w.W(k) .* (w.nu(k) - 2)));
+            % l = plot(t(:) .* nmsk(:), (x_hat(:) + 2.*s_mu) .* nmsk(:), ... 
+            %          '-', 'Color', c(k,:));
+            % l = plot(t(:) .* nmsk(:), (x_hat(:) - 2.*s_mu) .* nmsk(:), ...
+            %          '-', 'Color', c(k,:));
+
+            % % plot emission confidence intervals
+            % s = 1./sqrt(w.nu(k) * w.W(k));
+            % l = plot(t(:) .* nmsk(:), (x_hat(:)+2.*s) .* nmsk(:), ...
+            %          '-.', 'Color', c(k,:));
+            % l = plot(t(:) .* nmsk(:), (x_hat(:)-2.*s) .* nmsk(:), ...
+            %          '-.', 'Color', c(k,:));
+
+            % % append to labels
+            % mu_labels{k} = sprintf('mu_%d = %.2f +/- %.2f', k, w.mu(k), s_mu);
+            % sigma_labels{k} = sprintf('sigma_%d = %.2f +/- %.2f', k, s, 1./sqrt(2.*w.nu(k).*w.W(k).^2));
+            % Nk_labels{k} = sprintf('N_%d = %05.1f', k, sum(stat.gamma(:,k)));
+
+            % hold on;
+        end
+
+        hold off;
+    end
+
+
+    % plot raw signal
+    if handles.rawPlot
+        if length(d.don) > T
+            T = length(d.don);
+            t = 1:T;
+            axes(handles.plotAxes(1));
+            xlim([0, T]);
+        end
+        axes(handles.plotAxes(handles.rawPlot));
+        cla();
+        plot(t, d.don, 'g');
+        hold on;
+        plot(t, d.acc, 'r');
+        xlim([0, T]);
+        hold off;
+    end
+
+    % if length(fieldnames(d)) > 1
+    %     hold on;
+
+    %     % % loop over states
+    %     % c = 0.66 .* hsv(K);
+    %     % for k = 1:K
+    %     %     msk = (z_hat == k)
+    %     %     nmsk = ones(size(msk));
+    %     %     nmsk(~msk) = NaN;
+
+    %     %     % plot x_hat
+    %     %     l = plot(t(:) .* nmsk(:), x_hat(:) .* nmsk(:), '--', 'Color', c(k,:));
+    %     %     set(l, 'LineWidth', 1.5)
+
+    %     %     % plot mu posterior confidence intervals
+    %     %     s_mu = sqrt(1 ./ (w.beta(k) .* w.W(k) .* (w.nu(k) - 2)));
+    %     %     l = plot(t(:) .* nmsk(:), (x_hat(:) + 2.*s_mu) .* nmsk(:), ... 
+    %     %              '-', 'Color', c(k,:));
+    %     %     l = plot(t(:) .* nmsk(:), (x_hat(:) - 2.*s_mu) .* nmsk(:), ...
+    %     %              '-', 'Color', c(k,:));
+
+    %     %     % plot emission confidence intervals
+    %     %     s = 1./sqrt(w.nu(k) * w.W(k));
+    %     %     l = plot(t(:) .* nmsk(:), (x_hat(:)+2.*s) .* nmsk(:), ...
+    %     %              '-.', 'Color', c(k,:));
+    %     %     l = plot(t(:) .* nmsk(:), (x_hat(:)-2.*s) .* nmsk(:), ...
+    %     %              '-.', 'Color', c(k,:));
+
+    %     %     % append to labels
+    %     %     mu_labels{k} = sprintf('mu_%d = %.2f +/- %.2f', k, w.mu(k), s_mu);
+    %     %     sigma_labels{k} = sprintf('sigma_%d = %.2f +/- %.2f', k, s, 1./sqrt(2.*w.nu(k).*w.W(k).^2));
+    %     %     Nk_labels{k} = sprintf('N_%d = %05.1f', k, sum(stat.gamma(:,k)));
+
+    %     %     hold on;
+    %     % end
+    % end
+
+
+
+    % % add text labels
+    % t = text(0.05*T, 1.10, sprintf('L/T = %.1f', L(end)/T), 'FontSize', 14);
+    % t = text(0.60*T, 1, Nk_labels, 'FontSize', 14);
+    % t = text(0.70*T, 1, mu_labels, 'FontSize', 14);
+    % t = text(0.85*T, 1, sigma_labels, 'FontSize', 14);
+
 
 % --- Executes on slider movement.
 function plotSlider_Callback(hObject, eventdata, handles)
@@ -199,3 +273,26 @@ function plotEdit_CreateFcn(hObject, eventdata, handles)
       set(hObject,'BackgroundColor','white');
     end
     % scatter(t, FRET, 8, [0,0,0]);
+
+
+% --- Executes on button press in prevButton.
+function prevButton_Callback(hObject, eventdata, handles)
+% hObject    handle to prevButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if handles.curPlot > 1
+    handles.curPlot = handles.curPlot - 1;
+    guidata(hObject, handles);
+    tracePlotterGUI_PlotChanged(hObject, eventdata, handles);    
+end
+
+% --- Executes on button press in nextButton.
+function nextButton_Callback(hObject, eventdata, handles)
+% hObject    handle to nextButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if handles.curPlot < handles.maxPlot
+    handles.curPlot = handles.curPlot + 1;
+    guidata(hObject, handles);
+    tracePlotterGUI_PlotChanged(hObject, eventdata, handles);    
+end
