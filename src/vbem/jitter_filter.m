92a075b5ae2ac834183c9b042dc6e729e986c012
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2012-02-21 15:04:08 -0500

    jitter_filter.m: options.ignore -> ignore

--- src/vbem/jitter_filter.m
+++ src/vbem/jitter_filter.m
@@ -1,4 +1,4 @@
-function [g_f, xi_f, bad] = jitter_filter(z_hat, g, xi, options)
+function [g_f, xi_f, bad] = jitter_filter(z_hat, g, xi, ignore)
     [T K] = size(g);
     bad = find((z_hat(3:end-1) ~= z_hat(2:end-2)) ...
                & (z_hat(1:end-3) ~= z_hat(2:end-2))) + 1;
@@ -6,12 +6,12 @@ function [g_f, xi_f, bad] = jitter_filter(z_hat, g, xi, options)
     xi_f = xi;
     tdxs = [];
     % fix one point jumps
-    if strcmp(options.ignore, 'spike') + strcmp(options.ignore, 'all')
+    if strcmp(ignore, 'spike') + strcmp(ignore, 'all')
         % time points with spikes
         tdxs = bad(z_hat(bad-1) == z_hat(bad+1));
     end
     % fix one point intermediates
-    if strcmp(options.ignore, 'intermediate') + strcmp(options.ignore, 'all')
+    if strcmp(ignore, 'intermediate') + strcmp(ignore, 'all')
         % time points with spikes
         tdxs = cat(1, tdxs, bad(z_hat(bad-1) ~= z_hat(bad+1)));
     end
