25307a10132af06c6af2b1520ddbbd06abbdb6e3
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2012-02-21 15:12:33 -0500

    added: var_dir.m
    Calculates variances of a Dirichlet distribution

---
+++ util/var_dir.m
@@ -0,0 +1,9 @@
+function V = var_dir(alpha)
+	% V = var_dir(alpha)
+	%
+	% Variance of a Dirichlet prior
+	d = ndims(alpha);
+	K = size(alpha, d);
+	alpha0 = sum(alpha, d);
+	Alpha0 = alpha0.^2 .* (alpha0 - 1);
+	V = bsxfun(@times, alpha0 ./ Alpha0, bsxfun(@minus, alpha0, alpha));
