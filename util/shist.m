914876187d7f7f21109a1ca3d56f84d6dd82bd0a
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2012-02-23 17:16:50 -0500

    added: shist.m
    Calculates a smoothed histogram, using a set of standard errors
    for each observation

---
+++ util/shist.m
@@ -0,0 +1,26 @@
+function [h, b] =  shist(data, err, bins, wt)
+	% [h, b] =  shist(data, err, bins, wt)
+	%
+	% Calculates a smoothed histogram based on a set of datapoints
+	% with associated standard errors 
+	if length(bins) == 1
+		[h bins] = hist(data, bins);
+	end
+	if nargin < 4
+		wt = 1;
+	end
+	% ensure proper orientation with vector input
+	if max(size(data)) == length(data(:))
+		data = data(:)';
+		err = err(:)';
+		wt = wt(:)';
+	end
+	h = zeros(length(bins), size(data,1));
+	for d = 1:size(data,1)
+		pd = pd_norm(bins(:), data(d,:), err(d,:));
+		% normalize distribution over bins
+		pd = normalize(pd, 1);
+		% histogram is sum over all traces
+		h(:,d) = sum(bsxfun(@times, wt, pd), 2);
+	end
+	b = bins(:);
