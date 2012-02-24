2e6932cc5835892e3278f4f5d5a1e19759031380
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2012-02-23 20:41:22 -0500

    pd_dir.m: calculate in log to avoid over/underflow

--- stat/pd_dir.m
+++ stat/pd_dir.m
@@ -4,8 +4,9 @@ function pd =  pd_dir(x, alpha)
 	% Probability density function for a Dirichlet distribution
 
 	% figure out summation dim
-	d = ndims(alpha_p);
-	d = d - (size(alpha_p, d) == 1);
+	d = ndims(alpha);
+	d = d - (size(alpha, d) == 1);
 
-	Beta = @(alpha, d) prod(gamma(alpha), d)) ./ gamma(sum(alpha, d);
-	pd = 1./Beta(alpha, d) * prod(x.^(alpha-1), d);
+	ln_Beta = @(alpha, d) sum(gammaln(alpha), d) - gammaln(sum(alpha, d));
+	ln_pd = -ln_Beta(alpha, d) + sum(bsxfun(@times, log(x), (alpha-1)), d);
+	pd = exp(ln_pd);
