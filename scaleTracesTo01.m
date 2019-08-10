5d8c3b79c9043ba9baee16053250d830abaca18f
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-08-10 14:41:51 +0800

    scaleTracesTo01: move the minimum to 0 to avoid overscaling

--- scaleTracesTo01.m
+++ scaleTracesTo01.m
@@ -7,13 +7,17 @@ function [tracesOut, scale] = scaleTracesTo01(traces)
 tracesOut = zeros(size(traces));
 scale = zeros(nMolecules,2);
 for iMolecule = 1:nMolecules
-Imax = max(traces(iMolecule,:));
-Imin = min(traces(iMolecule,:));
-% y = mx + b
-mi = (Imax-Imin)/(0.9-0.1);
-bi = Imin - 0.1*mi;
-tracesOut(iMolecule, :) = (traces(iMolecule,:)-bi)/mi;
-scale(iMolecule, :) = [mi, bi];
+    Imax = max(traces(iMolecule,:));
+    % put the minimum to either 0 or the negative value
+    Imin = min(traces(iMolecule,:));
+    if Imin > 0
+        Imin = 0;
+    end
+    % y = mx + b
+    mi = (Imax-Imin)/(0.9-0.1);
+    bi = Imin - 0.1*mi;
+    tracesOut(iMolecule, :) = (traces(iMolecule,:)-bi)/mi;
+    scale(iMolecule, :) = [mi, bi];
 end
 
 
