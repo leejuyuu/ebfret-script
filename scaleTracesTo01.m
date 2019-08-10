4f48e4b9c3ada592e5db5038a55c3ca76cf695dc
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-08-10 13:35:15 +0800

    Add a function to scale individual trace intensity to [0,1]

---
+++ scaleTracesTo01.m
@@ -0,0 +1,20 @@
+function [tracesOut, scale] = scaleTracesTo01(traces)
+% Make fake traces for ebfret fitting
+% Pull individual traces in to range by linear scaling max--> 0.9, min
+% -->0.1
+% Traces (nMolecules x nFrames) double
+[nMolecules,~] = size(traces);
+tracesOut = zeros(size(traces));
+scale = zeros(nMolecules,2);
+for iMolecule = 1:nMolecules
+Imax = max(traces(iMolecule,:));
+Imin = min(traces(iMolecule,:));
+% y = mx + b
+mi = (Imax-Imin)/(0.9-0.1);
+bi = Imin - 0.1*mi;
+tracesOut(iMolecule, :) = (traces(iMolecule,:)-bi)/mi;
+scale(iMolecule, :) = [mi, bi];
+end
+
+
+end
