3e595aecf74ac2783a0cdb8ff8efbd13db1138ce
Tzu-Yu Lee <leejuyuu@gmail.com>
2019-08-11 13:27:13 +0800

    selectK.m: selecting best K using two criteria
    criterion 1: states should be separated by 3 sigma
    criterion 2: total time in a state should > 40 frames

---
+++ selectK.m
@@ -0,0 +1,45 @@
+function [newVb, newVit, selection] = selectK(runs)
+% load('runstemp0810.mat');
+maxK = length(runs(:,1));
+nMols = length(runs(1).vb);
+selection = ones(1,nMols);
+for iMol = 1:nMols
+    for iK = maxK:-1:1
+        
+        % Criterion 1: the fitted states should be separated by at least 3
+        % sigma 
+        sigma = 1./(sqrt(runs(iK).vb(iMol).w.W.*runs(iK).vb(iMol).w.nu));
+        mu = runs(iK).vb(iMol).w.mu;
+        if any(mu(1:end-1)+3*sigma(1:end-1) >= mu(2:end)) ||...
+                any(mu(2:end)-3*sigma(2:end) <= mu(1:end-1))                
+            continue
+        else
+            % Criterion 2: any fitted state should occupy at least 40
+            % frames in total.
+            isSmallOccupancy = false;
+            for i = 1:iK
+                isSmallOccupancy = sum(runs(iK).vit(iMol).z == i) < 40;
+            if isSmallOccupancy
+                break
+            end
+            end
+            if isSmallOccupancy
+                continue
+            end
+            
+            selection(iMol) = iK;
+            break
+        end
+    end
+end
+newVb = runs(1).vb;
+newVit = runs(1).vit;
+
+for i = 1:maxK
+    equalK = selection == i;
+    newVb(equalK) = runs(i).vb(equalK);
+    newVit(equalK) = runs(i).vit(equalK);
+    
+end
+   
+end
